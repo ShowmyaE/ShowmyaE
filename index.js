d9c160e80f3b763bec05c0ad804e63177ed22c66
showmyaE <showmya3@gmail.com>
2024-07-29 10:07:47 +0530

    add

--- index.js
+++ index.js
@@ -1,56 +1,119 @@
-const express = require('express')
-const app = express()
-const sqlite3 = require('sqlite3')
-const {open} = require('sqlite')
-const path = require('path')
+const express=require('express')
+const app=express()
+const sqlite3=require('sqlite3')
+const {open}=require('sqlite')
+const path=require('path')
+const cors = require("cors");
+const { v4: uuidv4 } = require('uuid'); 
+const jwt = require('jsonwebtoken')
 
-const dbPath = path.join(__dirname, 'demodb.db')
-let db = null
+app.use(cors());
+app.use(express.json())
 
-// app.use(express.json())
+const dbPath=path.join(__dirname,'demodb.db')
+let db=null
 
-app.use((req, res, next) => {
-  res.append('Access-Control-Allow-Origin', ['*']);
-  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.append('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+const initializeDbAndServer=async()=>{
+    try{
+        db=await open({
+            filename:dbPath,
+            driver:sqlite3.Database
+        })
+        app.listen(4000,()=>{
+          console.log('Server Running at http://localhost:4000')
+           
+        })
 
-const initializeDbAndServer = async () => {
-  try {
-    db = await open({
-      filename: dbPath,
-      driver: sqlite3.Database,
-    })
-
-    app.listen(3000, () => {
-      console.log('Server Running at http://localhost:3000/')
-    })
-  } catch (error) {
-    console.log(`DB Error : ${error.message}`)
-    process.exit(1)
+    }catch(error){
+        console.log(`DB Error : ${error.message}`)
+        process.exit(1)
   }
 }
 initializeDbAndServer()
+app.get('/',async(req,res)=>{
+    const getUserQuery=`select * from registration`;
+    const userDbDetails=await db.all(getUserQuery);
+    console.log('DB value',userDbDetails);
+    res.send(userDbDetails)
 
-app.get('/', async (req, res) => {
-    // const getUserQuery = 'create table google_keep(id INT PRIMARY KEY, title VARCHAR(50), description VARCHAR(500), date VARCHAR(25));'
-    // const getUserQuery = "INSERT INTO google_keep (id, title, description, date) VALUES (1, 'cardDetails', 'axis bank details', 'July 18, 2024');"
-  const getUserQuery = `select * from google_keep;`
-  const userDbDetails = await db.all(getUserQuery)
-  console.log("DB Value", userDbDetails)
-  res.send(userDbDetails)
+})
+const authentication = (request, response, next) => {
+    let jwtToken
+    console.log("AUTH", request.headers['authorization'])
+    const authHeader = request.headers['authorization']
+    if (authHeader) {
+      jwtToken = authHeader.split(' ')[1]
+    }
+    console.log("JWT", jwtToken)
+
+    if (jwtToken) {
+      jwt.verify(jwtToken, 'SECRET_KEY', (error, payload) => {
+        if (error) {
+          response.status(401)
+          response.send('Invalid JWT Token')
+        } else {
+          request.email = payload.email
+          request.userId = payload.userId
+          next()
+        }
+      })
+    } else {
+      response.status(401)
+      response.send('Invalid JWT Token')
+    }
+  }
+  
+
+app.post("/signUp",async(req,res)=>{
+    const {email, password} = req.body
+    console.log('DB value',email,password)
+    const id = uuidv4();
+     const insertquery=`INSERT INTO registration(CustomerId, EmailId, Password)
+    VALUES ('${id}', '${email}','${password}')`;
+    const insertData=await db.run(insertquery)
+if(insertData) {
+    res.send({data:"successfully inserted"})
+}
+else{
+    res.status(400)
+    res.send({data:"Failed to inserted"})
+}
 })
 
-app.post('/insertCard', async (req, res) => {
-  const {title, description, date} = req.body
-    const getUserQuery = `select * from google_keep;`
-  const getNoteLength = await db.all(getUserQuery)
-const insertNotes = `INSERT INTO google_keep (id, title, description, date) VALUES('${getNoteLength.length+1}','${title}','${description}','${date}');`
-console.log("INSERT", insertNotes)
-const userDbDetails = await db.run(insertNotes)
-console.log("DB Value", userDbDetails)
-res.send(userDbDetails)
+app.post("/loginIn",async(req,res)=>{
+    const {email, password} = req.body
+    console.log('DB value',email,password)
+    const getUserQuery = `SELECT * FROM registration WHERE EmailId='${email}' AND Password='${password}';`
+    const userDbDetails = await db.get(getUserQuery)
+    console.log(userDbDetails)
+    if (userDbDetails) {
+        const payload = {email:email,userId:userDbDetails.CustomerId}
+        const jwtToken = jwt.sign(payload, 'SECRET_KEY')
+        res.send({jwtToken})
+      } else {
+        res.status(400)
+        res.send('Invalid password')
+      }
 })
 
-// app.listen(3000)
+
+
+app.post('/bookingDetail', authentication, async(req,res) => {
+    const id = uuidv4();
+    const customerId=req.userId
+    console.log("req.body", req.body)
+    const {name,email, company,phonenumber,domain,interests, bookingDate} = req.body
+    const insertquery=`INSERT INTO bookingDetail (Id, CustomerId,Name,EmailId,Company,PhoneNumber,Domain,Interests,BookingDate,Date)
+    VALUES ('${id}', '${customerId}','${name}','${email}','${company}','${phonenumber}','${domain}','${interests}','${bookingDate}','${bookingDate}' )`;
+    const userDbDetails=await db.run(insertquery)
+    console.log("INSERTQUERY", userDbDetails)
+    res.send("SUCCESS")
+})
+
+app.get('/getbookingDetail', authentication, async (req,res) => {
+    const customerId=req.userId
+     const getUserQuery = `SELECT * FROM bookingDetail WHERE CustomerId='${customerId}' ;`
+     const insertData=await db.all(getUserQuery)
+     console.log("GET CUSTOMER", insertData)
+    res.send(insertData)
+})
